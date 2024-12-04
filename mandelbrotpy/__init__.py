f951f0dc84045650b81d49acd4e160cb8c9a27ca
YaxinerZhang <23373130@buaa.edu.cn>
2024-12-05 00:07:57 +0800

    Delete all files from the repository

--- mandelbrotpy/__init__.py
+++
@@ -1,5 +0,0 @@
-from matplotlib.projections import register_projection
-from .mandelbrot_class import MandelbrotSet
-from .mandelbrot_plot import MandelbrotAxes
-
-register_projection(MandelbrotAxes)
